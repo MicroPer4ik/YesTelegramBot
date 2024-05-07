855e23590085a34f58fe5cad5ad3e83aa283e58f
MicroPer4ik <balyakin.danil19@gmail.com>
2024-05-07 23:47:17 +0500

    Start

---
+++ YesTelegramBot/Commands.cs
@@ -0,0 +1,16 @@
+﻿using PRTelegramBot.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace YesTelegramBot
+{
+    public class Commands
+    {
+        
+    }
+}
--- YesTelegramBot/Program.cs
+++ YesTelegramBot/Program.cs
@@ -1,17 +1,32 @@
-﻿const string EXIT_COMMAND = "exit";
+﻿using PRTelegramBot.Core;
 
-try
+const string EXIT_COMMAND = "exit";
+
+var telegram = new PRBot(option =>
 {
-    while (true)
-    {
-        var result = Console.ReadLine();
-        if(result.ToLower() == EXIT_COMMAND)
-        {
-            Environment.Exit(0);
-        }
-    }
-}
-catch (Exception ex)
+    option.Token = "7081331580:AAGfc4owithTBeoxBU7EN81jyHf6MelEiBQ";
+    option.ClearUpdatesOnStart = true;
+    option.WhiteListUsers = new List<long> { };
+    option.Admins = new List<long> { };
+    option.BotId = 0;
+});
+
+telegram.OnLogCommon += Telegram_OnLogCommon;
+telegram.OnLogError += Telegram_OnLogError;
+
+await telegram.Start();
+void Telegram_OnLogError(Exception ex, long? id)
 {
-    Console.WriteLine(ex.Message);
+    Console.ForegroundColor = ConsoleColor.Red;
+    string errorMsg = $"{DateTime.Now}: {ex}";
+    Console.WriteLine(errorMsg);
+    Console.ResetColor();    
 }
+void Telegram_OnLogCommon(string msg, Enum typeEvent, ConsoleColor color)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string message = $"{DateTime.Now}:{msg}";
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
+
