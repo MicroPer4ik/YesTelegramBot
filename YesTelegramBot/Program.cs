3dcfa209e2288a671a1e67226ce7d43b9ae9fa08
Danil <Danil@HOME-PC>
2024-05-07 14:03:46 +0500

    Добавьте файлы проекта.

---
+++ YesTelegramBot/Program.cs
@@ -0,0 +1,17 @@
+﻿const string EXIT_COMMAND = "exit";
+
+try
+{
+    while (true)
+    {
+        var result = Console.ReadLine();
+        if(result.ToLower() == EXIT_COMMAND)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
